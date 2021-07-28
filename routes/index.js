e75e256d29485729bb89ce05182e4812466365b8
2Gunz <mlujan476@gmail.com>
2021-07-28 12:13:06 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-//database connect
+//DB connection
 function connectDb() {
     var conn = mysql.createConnection({
         host: "migo.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
@@ -12,27 +12,132 @@ function connectDb() {
     });
     return conn;
 }
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+//Proxy actually updates db
+function updateTable(id, table, column, val) {
     var conn = connectDb();
 
     conn.connect((err) => {
-
         if (err) throw err;
-        sql = "SELECT * FROM set_points";
-        var rows;
 
+        const sql = "UPDATE " + table + " SET " + column + " = " + val + " WHERE id = " + id;
+        conn.query(sql, (err, result) => {
+            if (err) res.send("\r\n Failed\r\n");
+
+            res.send("Data updated :)");
+        });
+
+    });
+}
+
+
+//Get table: set_points
+router.get('/set-points', function(req, res, next) {
+    var conn = connectDb();
+
+    conn.connect((err) => {
+        if (err) throw err;
+
+        const sql = "SELECT * FROM set_points";
+        var rows;
 
         conn.query(sql, (err, result) => {
             if (err) res.send("\r\n Failed\r\n");
 
             rows = JSON.parse(JSON.stringify(result[result.length - 1]));
             console.table(rows);
-            res.send(rows["temp1"] + " " + rows["temp2"]);
+            res.send(rows);
         });
     });
 
 
 });
+
+
+//Update table: set_points
+router.post('/set-points', (req, res, next) => {
+
+    var id = req.params.id;
+    id = parseInt(id);
+
+    const table = "set_points";
+    if (req.params.temp1 !== "") {
+        let val = parseFloat(req.params.temp1);
+        let column = "temp1";
+        updateTable(id, table, column, val);
+
+
+    }
+
+    if (req.params.temp2 !== "") {
+        let val = parseFloat(req.params.temp2);
+        let column = "temp2";
+        updateTable(id, table, column, val);
+
+
+    }
+
+    if (req.params.time1 !== "") {
+        let val = req.params.time1;
+        let column = "time1";
+        updateTable(id, table, column, val);
+
+
+    }
+
+    if (req.params.time2 !== "") {
+        let val = req.params.time2;
+        let column = "time2";
+        updateTable(id, table, column, val);
+
+
+    }
+
+    if (req.params.time3 !== "") {
+        let val = req.params.time3;
+        let column = "time3";
+        updateTable(id, table, column, val);
+
+
+    }
+});
+
+//Get table: action
+router.get('/action', (req, res, next) => {
+    var conn = connectDb();
+
+    conn.connect((err) => {
+        if (err) throw err;
+
+        const sql = "SELECT * FROM action";
+
+        conn.query(sql, (err, result) => {
+            if (err) res.send("\r\n Failed\r\n");
+
+            var rows = JSON.parse(JSON.stringify(result[result.length - 1]));
+            console.table(rows);
+            res.send(rows);
+        });
+    });
+})
+
+//Update table: action
+router.post('/action', (req, res, next) => {
+
+    var id = 1;
+    const table = "action";
+    if (req.params.status) {
+        let val = req.params.status;
+        let column = "status";
+        updateTable(id, table, column, val);
+    }
+
+    if (req.params.time) {
+        let val = req.params.time;
+        let column = "time";
+        updateTable(id, table, column, val);
+    }
+
+})
 
 module.exports = router;
