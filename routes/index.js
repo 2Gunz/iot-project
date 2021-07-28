1779890a6bf14d8379454c315a22b43366dd4856
2Gunz <mlujan476@gmail.com>
2021-07-27 18:19:04 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,34 @@ var router = express.Router();
 var mysql = require('mysql');
 
 //database connect
-function connectDb()
-{
-  var conn = mysql.createConnection({
-    host: "migo.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
-    user: "migo",
-    password: "migomigo",
-    database: "mydb",
-  });
-  return conn;
+function connectDb() {
+    var conn = mysql.createConnection({
+        host: "migo.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
+        user: "migo",
+        password: "migomigo",
+        database: "iot_project",
+    });
+    return conn;
 }
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var conn = connectDb();
+    var conn = connectDb();
 
-  if(conn)
-    {
-      
-      conn.connect((err) => {
+    conn.connect((err) => {
+
         if (err) throw err;
         const sql = "SELECT * FROM set_points";
         var rows;
-        conn.query(sql, (err, result) => {
-          if (err) res.send("\r\n Failed\r\n");
-          rows = JSON.parse(JSON.stringify(result));
-          res.send("\r\n"+result.body.temp1+"\r\n");
-        });
-      });
 
-    }
+
+        conn.query(sql, (err, result) => {
+            if (err) res.send("\r\n Failed\r\n");
+            rows = JSON.parse(JSON.stringify(result));
+            res.send("\r\n" + rows.body.temp1 + "\r\n");
+        });
+    });
+
+
 });
 
 module.exports = router;
-
-
