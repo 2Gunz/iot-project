c7331d40d25dded132b7e786913a16ba23239f11
2Gunz <mlujan476@gmail.com>
2021-07-27 18:10:39 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,36 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+//database connect
+function connectDb()
+{
   var conn = mysql.createConnection({
     host: "migo.cym4s4x6gfpj.us-east-2.rds.amazonaws.com",
     user: "migo",
     password: "migomigo",
     database: "mydb",
   });
+  return conn;
+}
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var conn = connectDb();
 
   if(conn)
-    res.send("connected to db");
-  else
-    res.send("not connected");
+    {
+      
+      conn.connect((err) => {
+        if (err) throw err;
+        const sql = "SELECT * FROM set_points";
+        var rows;
+        conn.query(sql, (err, result) => {
+          if (err) res.send("\r\n Failed\r\n");
+          rows = JSON.parse(JSON.stringify(result));
+          res.send("\r\n"+result.body.temp1+"\r\n");
+        });
+      });
+
+    }
 });
 
 module.exports = router;
