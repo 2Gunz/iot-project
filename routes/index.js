1c97221afe13611dd9945a360b4159fb2b2f0831
2Gunz <mlujan476@gmail.com>
2021-07-30 19:53:58 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -179,6 +179,8 @@ router.post("/get-status", (req, res, next) => {
   currentTime = parseInt(currentTime);
 
 
+  const j = JSON.stringify({"status":currentTime, "dateTime": setPoints[0]["time1"]});
+  res.send(j);
   
   var currentTemperature = req.body.temp;
 
