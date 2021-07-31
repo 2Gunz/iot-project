964bee3b4111cbe4077f044da41e3573c5a845ea
2Gunz <mlujan476@gmail.com>
2021-07-31 15:37:23 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -380,49 +380,53 @@ router.post("/", (req,res,next) =>{
     {
         res.render("index", {message: "You entered bad values, try again..."})
     }
-
-    time1 = time1.split(":");
-    time1 = time1[0] + time1[1] + "00";
-
-    time2 = time2.split(":");
-    time2 = time2[0] + time2[1] + "00";
-
-    time3 = time3.split(":");
-    time3 = time3[0] + time3[1] + "00";
-
-    time1 = parseInt(time1);
-    time2 = parseInt(time2);
-    time3 = parseInt(time3);
+    else
+    {
+        time1 = time1.split(":");
+        time1 = time1[0] + time1[1] + "00";
+    
+        time2 = time2.split(":");
+        time2 = time2[0] + time2[1] + "00";
+    
+        time3 = time3.split(":");
+        time3 = time3[0] + time3[1] + "00";
+    
+        time1 = parseInt(time1);
+        time2 = parseInt(time2);
+        time3 = parseInt(time3);
+    
+        
+    
+        var sp1 = {"time": time1, "temp1": temp1, "temp2":temp2};
+        var sp2 = {"time": time2, "temp1": temp3, "temp2":temp4};
+        var sp3 = {"time": time3, "temp1": temp5, "temp2":temp6};
+    
+        var spArr = [sp1, sp2, sp3];
+    
+        spArr.sort((a,b) => (a.time < b.time) ? 1 : -1);
+    
+       var spCount = 1;
+       spArr.map((object, index) => {
+           
+        pool.getConnection((err, connection) => {
+            if (err) throw err;
+        
+            const sql = "UPDATE set_points SET time1 = " + object["time"] + ", temp1 = " + object["temp1"] + ", temp2 = " + object["temp2"] + " WHERE id = " + spCount;
+                
+            connection.query(sql, (err, result) => {
+              connection.release();
+              if (err) throw err;
+                
+              res.render("index");
+            });
+            spCount++;
+          });
+       })
+    
+    
+    }
 
     
-
-    var sp1 = {"time": time1, "temp1": temp1, "temp2":temp2};
-    var sp2 = {"time": time2, "temp1": temp3, "temp2":temp4};
-    var sp3 = {"time": time3, "temp1": temp5, "temp2":temp6};
-
-    var spArr = [sp1, sp2, sp3];
-
-    spArr.sort((a,b) => (a.time < b.time) ? 1 : -1);
-
-   var spCount = 1;
-   spArr.map((object, index) => {
-       
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-    
-        const sql = "UPDATE set_points SET time1 = " + object["time"] + ", temp1 = " + object["temp1"] + ", temp2 = " + object["temp2"] + " WHERE id = " + spCount;
-            
-        connection.query(sql, (err, result) => {
-          connection.release();
-          if (err) throw err;
-            
-          res.render("index");
-        });
-        spCount++;
-      });
-   })
-
-
 
 
 })
